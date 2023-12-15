dbaf575decafde1ada4a4a008d7e48e27c12cac4
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-12-15 15:41:32 +0530

    added router

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,29 @@
+import { Link } from "react-router-dom";
+
 const Navbar = ()=>{
     return (
         <div className=" flex justify-between p-4 bg-stone-200">
             <div>
+                <Link to={'/'}>
                 Logo
                 Title
+                </Link>
             </div>
-            <div>
-                links
+            <div className="flex justify-between">
+                <ul className="flex flex-wrap">
+                    <li className="m-2 p-1">MEN</li>
+                    <li className="m-2 p-1">WOMEN</li>
+                    <li className="m-2 p-1">KIDS</li>
+                    <li className="m-2 p-1">HOME & LIVING</li>
+                    <li className="m-2 p-1">BEAUTY</li>
+                    <li className="m-2 p-1">STUDIO</li>
+                </ul>
+                <input className="m-2 p-1 rounded-md w-96" placeholder="Search Here...."/>
+                <ul className="flex">
+                    <li className="m-2 p-1">Profile</li>
+                    <li className="m-2 p-1">Wishlist</li>
+                    <li className="m-2 p-1"><Link to={'/bag'}>Bag</Link></li>
+                </ul>
             </div>
         </div>
     )
--- src/app.js
+++ src/app.js
@@ -3,16 +3,37 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Footer from "./Footer";
+import {Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorElement from "./ErrorElement";
+import BagElement from "./BagElement.js";
 
 const Applayout = ()=>{
     return(
         <div className="flex flex-col h-screen">
         <Navbar/>
-        <Home/>
+        <Outlet/>
         <Footer/>
         </div>
     )
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<Applayout />,
+        errorElement:<ErrorElement/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            },
+            {
+                path:"/bag",
+                element:<BagElement/>
+            }
+        ]
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout></Applayout>)
+root.render(<RouterProvider router={appRouter}></RouterProvider>)
