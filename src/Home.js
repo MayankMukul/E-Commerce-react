037d6a04202f358b48a1ab3d42b5ad67dcf30a59
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-12-21 23:56:12 +0530

    added remove button

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import DATA from "../CONSTANT/CONST_DATA.js";
 import { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
 import { useDispatch } from "react-redux";
-import { addItem } from "./homePageSlice.js";
+import { addItem, removeItem } from "./homePageSlice.js";
 
 const Home =()=>{
     const [data, setdata]= useState([]);
@@ -15,21 +15,39 @@ const Home =()=>{
 
     let store = useSelector(store=>store.cart.items)
     // console.log(store);
+    
+    
 
     const handleAddItem = (item)=>{
         // console.log('itemclicked',item)
         dispatch(addItem(item));
     }
     
+    const handleRemoveItem = (item)=>{
+        // console.log('itemclicked',item)
+        dispatch(removeItem(item));
+    }
+
+    let items = useSelector(store=>store.cart.items)
+    
     const  ItemCard=(props)=>{
-        // console.log(props.item)
+        console.log(props.item.id)
+        console.log(items);
+        const elementFound = items.indexOf(props.item.id)>=0;
+        console.log(elementFound);
+
             return(
                 <div className="bg-stone-300 m-2 w-52">
                 <h1>Name : {props.item.item_name}</h1>
                 <p>Company : {props.item.company}</p>
                 <p>Price : {props.item.current_price}</p>
+                {elementFound?
+                <button className="bg-black text-white m-2 p-1 rounded w-2/3 "
+                onClick={()=>handleRemoveItem(props.item.id)}>Remove</button>
+                :
                 <button className="bg-black text-white m-2 p-1 rounded w-2/3 "
                 onClick={()=>handleAddItem(props.item.id)}>Add</button>
+                }
                 </div>
             )
         
