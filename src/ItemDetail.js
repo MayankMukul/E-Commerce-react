efbccb071a85cc70feb151d09de06409c695936b
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-01-02 23:04:03 +0530

    added store to item page

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -1,16 +1,23 @@
 import { useParams } from "react-router-dom"; 
-import {useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+// import { useDispatch } from 'react-redux';
+import { addItem } from "./homePageSlice";
 
 function ItemDetail() {
     const { item_id } = useParams();
-    console.log(item_id)
+    // console.log(item_id)
     const list  = useSelector(store =>store.itemList.items );
     // console.log(list);
     const item = list.filter(item=>{
         return item.id == Number(item_id);
     });
-    console.log(item[0]);
-    console.log(item[0].rating);
+
+    const dispatch = useDispatch();
+    const handleaddItem=()=>{
+        dispatch(addItem(item[0].id))
+        console.log(item[0].id);
+    }
+    
 
     return ( 
         <div className="container m-auto p-2 bg-stone-300">
@@ -21,6 +28,9 @@ function ItemDetail() {
         <p>Original Price : {item[0].original_price}</p>
         <p>Current Price : {item[0].current_price}</p>
         <p>Rating : {item[0].rating.stars}</p>
+        <button className="bg-black text-white rounded m-1 p-1" 
+        onClick={()=>handleaddItem()}
+        >Add to cart</button>
         </div>
      );
 }
