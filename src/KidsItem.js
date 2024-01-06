4096152fe978235b737d44b18d741df5a7b9d599
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-01-06 13:03:30 +0530

    kids page added

---
+++ src/KidsItem.js
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import ItemCard from './ItemCard';
+
+function KidsItems() {
+    const itemslist = useSelector(store =>store.itemList.items );
+
+    const mensItemList = itemslist.filter(item=>item.tags=='kids');
+
+    return ( 
+        <div className="container m-auto bg-stone-50">
+            <h1 className="text-center my-5">Kids Items</h1>
+            <div className="flex flex-wrap justify-center m-auto">
+          {mensItemList.map((item) => {
+            return <ItemCard key={item.id} item={item} />;
+          })}
+        </div>
+        </div>
+     );
+}
+
+export default KidsItems;
