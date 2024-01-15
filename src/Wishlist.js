1d90c8bcf90d7e3418039026504d23e150386b03
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-01-15 17:14:34 +0530

    clear button added

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import ItemCard from './ItemCard';
+import { clearWishList } from './wishlistSlice';
+
 
 export default function Wishlist() {
     const wishlist = useSelector(store => store.wishList.items);
     // console.log(wishlist);
     const storeList = useSelector(store=> store.itemList.items);
-    // console.log(storeList)
+    // console.log(storeList);
+    const cart  = useSelector(store=>store.cart.items);
+    // console.log(cart);
 
     const itemOnWishlist = storeList.filter(item=>{
         let index = wishlist.indexOf(item.id);
         return (index>=0)?true:false;
     });
     // console.log(itemOnWishlist)
+
+    const dispatch = useDispatch()
+
+    const handleClearwishlist=()=>{
+      dispatch(clearWishList());
+    }
     
   return (
     <div className="m-auto w-2/3 bg-stone-100">
@@ -23,12 +33,10 @@ export default function Wishlist() {
         })}
       </div>
       {wishlist.length > 0 ? (
-        <>
-        <button className='bg-black text-white rounded p-1 float-right m-4'>Add to Cart</button>
-        <button className='bg-black text-white rounded p-1 float-right m-4'>Clear Wishlist</button>
-        </>
+        <button className='bg-black text-white rounded p-1 float-right m-4' 
+        onClick={()=>handleClearwishlist()}>Clear Wishlist</button>
       ) : (
-        <h1 className='italic text-center m-1 p-1'>Add item to wishlist.</h1>
+        <h1 className='italic text-center m-1 p-1'>Wishlist Empty.</h1>
       )}
     </div>
   );
