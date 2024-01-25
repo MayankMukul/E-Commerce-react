fba2a7f48c9d19fcf4de5a8dd936eeffba9633c9
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-01-25 13:05:30 +0530

    nav update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+
 
 const Navbar = ()=>{
     let items = useSelector(store=>store.cart.items)
@@ -14,18 +15,18 @@ const Navbar = ()=>{
             </div>
             <div className="flex justify-between">
                 <ul className="flex flex-wrap">
-                    <li className="m-2 p-1">MEN</li>
-                    <li className="m-2 p-1">WOMEN</li>
-                    <li className="m-2 p-1">KIDS</li>
-                    <li className="m-2 p-1">HOME & LIVING</li>
+                    <li className="m-2 p-1"><Link to={'/'}>HOME</Link></li>
+                    <li className="m-2 p-1"><Link to={'/men'}>MEN</Link></li>
+                    <li className="m-2 p-1"><Link to={'/women'}>WOMEN</Link></li>
+                    <li className="m-2 p-1"><Link to={'/kids'}>KIDS</Link></li>
                     <li className="m-2 p-1">BEAUTY</li>
                     <li className="m-2 p-1">STUDIO</li>
                 </ul>
                 <input className="m-2 p-1 rounded-md w-96" placeholder="Search Here...."/>
                 <ul className="flex">
-                    <li className="m-2 p-1">Profile</li>
-                    <li className="m-2 p-1">Wishlist</li>
-                    <li className="m-2 p-1"><Link to={'/bag'}>Bag<span className="bg-red-500 text-white p-1 rounded-full">{items.length}</span></Link></li>
+                    <li className="m-2 p-1 text-center"><Link to={'/profile'}>Profile</Link></li>
+                    <li className="m-2 p-1"><Link to={'/wishlist'}>Wishlist</Link></li>
+                    <li className="m-2 p-1"><Link to={'/bag'}><span>Bag</span><span className="bg-red-500 text-white p-1 rounded-full">{items.length}</span></Link></li>
                 </ul>
             </div>
         </div>
