b8d2c4ae2088fede8522bc09097dec0a46f9f404
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-02-03 21:33:01 +0530

    search button added

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,11 +3,16 @@ import { useSelector } from 'react-redux';
 import { IoBagHandleSharp } from "react-icons/io5";
 import { IoPerson } from "react-icons/io5";
 import { GoHeartFill } from "react-icons/go";
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
 
 
 const Navbar = ()=>{
     let items = useSelector(store=>store.cart.items)
     // console.log(items.length);
+
+    const [searchText, setsearchText] = useState('');
+    console.log(searchText);
 
     return (
         <div className=" flex justify-between p-4 bg-stone-200">
@@ -25,11 +30,19 @@ const Navbar = ()=>{
                     <li className="m-2 p-1">BEAUTY</li>
                     <li className="m-2 p-1">STUDIO</li>
                 </ul>
-                <input className="m-2 p-1 rounded-md w-96" placeholder="Search Here...."/>
+                <button className=" p-2 mt-1 bg-white text-black h-10 rounded-s-lg"
+                onClick={()=>{
+                    window.location='/search/'+ searchText;
+                }}><IoSearch/></button>
+                <input className="p-1 mr-2 mt-1 rounded-e-lg w-96 h-10 focus:outline-none" placeholder="Search Here...."
+                onChange={(text)=>{
+                    setsearchText(text.target.value);
+                }
+                }/>
                 <ul className="flex">
-                    <li className="m-auto p-1"><Link to={'/profile'}><IoPerson className="m-auto"/>Profile</Link></li>
-                    <li className="m-2 p-1"><Link to={'/wishlist'}><GoHeartFill className="m-auto"/>Wishlist</Link></li>
-                    <li className="m-2 p-1"><Link to={'/bag'}><span><IoBagHandleSharp className="m-auto"/>Bag</span>
+                    <li className="m-auto p-1 text-sm"><Link to={'/profile'}><IoPerson className="m-auto"/>Profile</Link></li>
+                    <li className="m-2 p-1 text-sm"><Link to={'/wishlist'}><GoHeartFill className="m-auto"/>Wishlist</Link></li>
+                    <li className="m-2 p-1 text-sm"><Link to={'/bag'}><span><IoBagHandleSharp className="m-auto"/>Bag</span>
                     {(items.length!=0)?
                     <span className="bg-red-500 text-white p-1 rounded-3xl">{items.length}</span>
                     :<></>}
