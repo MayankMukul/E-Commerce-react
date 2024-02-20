d5c83235bceb974189997efb924f1002613b0135
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-02-20 21:10:28 +0530

    profile update

--- src/Profile.js
+++ src/Profile.js
@@ -20,25 +20,25 @@ export default function Profile() {
       <div>
         <h2 className="text-3xl font-bold text-center">Profile Section</h2>
       </div>
-      <div className='text-center flex justify-center '>
-        <div className='m-2 bg-stone-300 w-1/3 border-4 rounded'>
+      <div className='text-center flex max-md:flex-col max-md: justify-center '>
+        <div className='m-2 bg-stone-300 w-1/3 max-md:w-11/12 border-4 rounded max-md:self-center'>
         <h1 className='w-full bg-black text-white rounded p-2 '>User Info</h1>
           <h1>Name : { profile.username } </h1>
           <h1>Email : { profile.email}</h1>
         </div>
 
-        <form className=" text-center bg-slate-100 w-1/3  my-2   border-4 rounded-lg">
+        <form className=" text-center bg-slate-100 w-1/3 max-md:w-11/12 my-2 max-md:self-center  border-4 rounded-lg">
             <h1 className='w-full bg-black text-white rounded p-2'>Profile</h1>
           <div className='m-2'>
             <label htmlFor='name' >Name </label>
-            <input id='name' className="m-1 p-1 rounded " value={profileName} placeholder='Enter Name here...' required
+            <input id='name' className="m-1 p-1 rounded w-2/3 max-sm:w-3/5" value={profileName} placeholder='Enter Name here...' required
             onChange={(e)=>{
               setprofileName(e.target.value )
             }}/>
           </div>
           <div className='m-2'>
             <label htmlFor='email'>Email Id </label>
-            <input id='email' className="m-1 p-1 rounded " value={profileEmail} placeholder='Enter Email here...' required  
+            <input id='email' className="m-1 p-1 rounded w-2/3 max-sm:w-3/5" value={profileEmail} placeholder='Enter Email here...' required  
             onChange={(e)=>{
               setprofileEmail(e.target.value )
             }}/>
