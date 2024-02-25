28c5dc7374b8d5239e96bff911619c9bc1af3f53
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-02-25 23:29:31 +0530

    modal

--- src/Profile.js
+++ src/Profile.js
@@ -8,10 +8,17 @@ export default function Profile() {
     const profile = useSelector(store => store.profile.items);
     const dispatch = useDispatch();
 
+    const [showModal, setModal] = useState(false);
+
+    const handleModal = ()=>{
+      setModal(!showModal)
+    }
+
     const handleClick=(e)=>{
-        e.preventDefault();
+      e.preventDefault();
         dispatch(setProfileUsername(profileName));
         dispatch(setProfileEmail(profileEmail));
+        handleModal();
       }
 
 
@@ -20,33 +27,67 @@ export default function Profile() {
       <div>
         <h2 className="text-3xl font-bold text-center">Profile Section</h2>
       </div>
-      <div className='text-center flex max-md:flex-col max-md: justify-center '>
-        <div className='m-2 bg-stone-300 w-1/3 max-md:w-11/12 border-4 rounded max-md:self-center'>
-        <h1 className='w-full bg-black text-white rounded p-2 '>User Info</h1>
-          <h1>Name : { profile.username } </h1>
-          <h1>Email : { profile.email}</h1>
+      <div className="text-center flex max-md:flex-col max-md: justify-center ">
+        <div className="m-2 bg-stone-300 w-1/3 max-md:w-11/12 border-4 rounded max-md:self-center">
+          <h1 className="w-full bg-black text-white rounded p-2 ">User Info</h1>
+          <h1>Name : {profile.username} </h1>
+          <h1>Email : {profile.email}</h1>
         </div>
 
-        <form className=" text-center bg-slate-100 w-1/3 max-md:w-11/12 my-2 max-md:self-center  border-4 rounded-lg">
-            <h1 className='w-full bg-black text-white rounded p-2'>Profile</h1>
-          <div className='m-2'>
-            <label htmlFor='name' >Name </label>
-            <input id='name' className="m-1 p-1 rounded w-2/3 max-sm:w-3/5" value={profileName} placeholder='Enter Name here...' required
-            onChange={(e)=>{
-              setprofileName(e.target.value )
-            }}/>
+        <form
+          className=" text-center bg-slate-100 w-1/3 max-md:w-11/12 my-2 max-md:self-center  border-4 rounded-lg"
+          onSubmit={(event) => {
+            handleClick(event);
+            return false;
+          }}
+        >
+          <h1 className="w-full bg-black text-white rounded p-2">Profile</h1>
+          <div className="m-2">
+            <label htmlFor="name">Name </label>
+            <input
+              id="name"
+              className="m-1 p-1 rounded w-2/3 max-sm:w-3/5"
+              value={profileName}
+              placeholder="Enter Name here..."
+              required
+              onChange={(e) => {
+                setprofileName(e.target.value);
+              }}
+            />
           </div>
-          <div className='m-2'>
-            <label htmlFor='email'>Email Id </label>
-            <input id='email' className="m-1 p-1 rounded w-2/3 max-sm:w-3/5" value={profileEmail} placeholder='Enter Email here...' required  
-            onChange={(e)=>{
-              setprofileEmail(e.target.value )
-            }}/>
+          <div className="m-2">
+            <label htmlFor="email">Email Id </label>
+            <input
+              id="email"
+              className="m-1 p-1 rounded w-2/3 max-sm:w-3/5"
+              value={profileEmail}
+              placeholder="Enter Email here..."
+              required
+              onChange={(e) => {
+                setprofileEmail(e.target.value);
+              }}
+            />
           </div>
-          <button className="bg-black hover:bg-gray-600 text-white p-1 m-2 rounded" onClick={(event)=>handleClick(event)}>SignUp</button>
+          <input
+            type="submit"
+            value="SignUp"
+            className="bg-black hover:bg-gray-600 text-white p-1 m-2 rounded"
+          />
         </form>
       </div>
-      
+
+      {showModal ? (
+        <div className="fixed top-0 left-0 w-full h-full  bg-black bg-opacity-70 ">
+          <div className=' m-auto  bg-white w-1/3 h-40 rounded shadow-lg'>
+            <h1 className='bg-black text-white p-2 rounded'>Signed Up Successfully!</h1>
+            <div className='m-2 text-center'>
+            <h1>Name : {profile.username} </h1>
+          <h1>Email : {profile.email}</h1>
+            </div>
+            <button className='bg-black text-white rounded p-1 m-1' onClick={() => handleModal()}>Close</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
