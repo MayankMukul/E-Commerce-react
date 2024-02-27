8d4c8b2d4a6a5f1828ad65ef323e6ae13d8f8457
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-02-27 23:23:01 +0530

    beauty page list added

--- src/BeautyItem.js
+++ src/BeautyItem.js
@@ -1,12 +1,24 @@
+import { useSelector } from 'react-redux';
+import ItemCard from './ItemCard';
+
 const BeautyItem = ()=>{
+
+    const itemslist = useSelector(store =>store.itemList.items );
+
+    const beautyItemList = itemslist.filter(item=>item.tags=='beauty');
+
     return (
-        <div className="container m-auto bg-stone-50">
-        <h1 className="text-center p-1 rounded bg-black text-white ">Beauty Items</h1>
+      <div className="container m-auto bg-stone-50">
+        <h1 className="text-center p-1 rounded bg-black text-white ">
+          Beauty Items
+        </h1>
         <div className="flex flex-wrap justify-center m-auto my-2">
-          This is Beauty Item Page. 
+          {beautyItemList.map((item) => {
+            return <ItemCard key={item.id} item={item} />;
+          })}
         </div>
       </div>
-    )
+    );
 }
 
 export default  BeautyItem;
