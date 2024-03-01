370a6e7376aa1e9a54922bf0a2305d26e9e8b390
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-01 23:41:38 +0530

    item detail update

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -29,29 +29,48 @@ function ItemDetail() {
     // console.log(elementFound);
     
 
-    return ( 
-        <div className="container m-auto p-2 bg-stone-300">
-        <h1>This is a item Page.</h1>
-        <p>{item[0].image}</p>
-        <p>Name : {item[0].item_name}</p>
-        <p>Company : {item[0].company}</p>
-        <p>Original Price : {item[0].original_price}</p>
-        <p>Current Price : {item[0].current_price}</p>
-        <p>Rating : {item[0].rating.stars}</p>
+    return (
+      <div className="container m-auto bg-stone-300">
+        <h1 className="text-xl md:font-bold bg-black text-white rounded p-1">
+          This is a item Page.
+        </h1>
+        <div className="flex">
+          <img
+            src={`${item[0].image}`}
+            alt="unable to fetch"
+            height="500"
+            width="400"
+            className="rounded p-1"
+          />
 
-        {
-            elementFound?
-            (<button className="bg-black text-white rounded m-1 p-1" 
-            onClick={()=>handleremoveItem()}
-            >Remove</button>
-            ):
-            (<button className="bg-black text-white rounded m-1 p-1" 
-            onClick={()=>handleaddItem()}
-            >Add to cart</button>
-            )
-        }
+          <div className="m-2 ">
+            <p>Name : {item[0].item_name}</p>
+            <p>Company : {item[0].company}</p>
+            <p>Original Price : {item[0].original_price}</p>
+            <p>Current Price : {item[0].current_price}</p>
+            <p>Rating : {item[0].rating.stars}</p>
+
+        {elementFound ? (
+          <button
+            className="bg-black text-white rounded m-1 p-1"
+            onClick={() => handleremoveItem()}
+          >
+            Remove
+          </button>
+        ) : (
+          <button
+            className="bg-black text-white rounded m-1 p-1"
+            onClick={() => handleaddItem()}
+          >
+            Add to cart
+          </button>
+        )}
+          </div>
         </div>
-     );
+        {/* <p>{item[0].image}</p> */}
+
+      </div>
+    );
 }
 
 export default ItemDetail;
