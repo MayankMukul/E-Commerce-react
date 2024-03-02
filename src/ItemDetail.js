c8c1d1e9724eab26a67d6770b16a312c587a330f
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-02 21:12:13 +0530

    wishlist added to item page

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useDispatch } from "react-redux";
 import { addItem,removeItem } from "./homePageSlice";
+import { addToWishList, removeFromWishList } from './wishlistSlice';
+import { GoHeart } from "react-icons/go";
 
 
 function ItemDetail() {
@@ -24,28 +26,38 @@ function ItemDetail() {
         // console.log(item[0].id);
     }
 
+    const handleAddtoWishlist = ()=>{
+      dispatch(addToWishList(item[0].id))
+    }
+
+    const handleRemovefromWishlist=()=>{
+      dispatch(removeFromWishList(item[0].id))
+    }
+
     let itemsincart = useSelector(store=>store.cart.items)
     const elementFound = itemsincart.indexOf(item_id)>=0;
+    const wishlist = useSelector(store => store.wishList.items);
+    const wishlistelementfound = wishlist.indexOf(item_id)>=0;
     // console.log(elementFound);
     
 
     return (
-      <div className="container m-auto bg-stone-300">
-        <h1 className="text-xl md:font-bold bg-black text-white rounded p-1">
-          This is a item Page.
-        </h1>
+      <div className="container m-auto bg-stone-300 p-3 rounded">
+        {/* <h1 className="text-xl md:font-bold bg-black text-white rounded p-2">
+          {item[0].company}
+        </h1> */}
         <div className="flex">
           <img
             src={`${item[0].image}`}
             alt="unable to fetch"
-            height="500"
-            width="400"
+            height="400"
+            width="350"
             className="rounded p-1"
           />
 
           <div className="m-2 ">
-            <p>Name : {item[0].item_name}</p>
-            <p>Company : {item[0].company}</p>
+            <p className="text-xl md:font-bold">{item[0].company}</p>
+            <p className="text-xl md:font-bold">{item[0].item_name}</p>
             <p>Original Price : {item[0].original_price}</p>
             <p>Current Price : {item[0].current_price}</p>
             <p>Rating : {item[0].rating.stars}</p>
@@ -65,6 +77,22 @@ function ItemDetail() {
             Add to cart
           </button>
         )}
+
+{wishlistelementfound ? (
+                <button
+                  className="bg-red-300 p-1 m-1 rounded-xl"
+                  onClick={() => handleRemovefromWishlist()}
+                >
+                  <GoHeart />
+                </button>
+              ) : (
+                <button
+                  className=" p-1 m-1 rounded"
+                  onClick={() => handleAddtoWishlist()}
+                >
+                  <GoHeart />
+                </button>
+              )}
           </div>
         </div>
         {/* <p>{item[0].image}</p> */}
