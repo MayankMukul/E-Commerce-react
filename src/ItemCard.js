51b3925664b9b4734ec0c95c420588ed9387f448
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-05 23:16:05 +0530

    item update

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -6,6 +6,7 @@ import { GoHeart } from "react-icons/go";
 
 const  ItemCard=(props)=>{
 
+  // console.log(props.item.image);
     let items = useSelector(store=>store.cart.items);
       const elementFound = items.indexOf(props.item.id)>=0;
 
@@ -29,42 +30,52 @@ const  ItemCard=(props)=>{
 
     const handleRemovefromWishlist=(item)=>{
       dispatch(removeFromWishList(item))
-      // console.log(item);
     }
 
           return (
-            <div className="bg-stone-300 m-2 w-52">
+            <div className="bg-stone-300 m-2 p-2 rounded w-52">
               <Link to={"/item/" + props.item.id}>
+                <img
+                  src={`${props.item.image}`}
+                  alt="unable to fetch"
+                  height="300"
+                  width="200"
+                  className="rounded"
+                />
                 <h1>Name : {props.item.item_name}</h1>
                 <p>Company : {props.item.company}</p>
                 <p>Price : {props.item.current_price}</p>
               </Link>
-                {elementFound ? (
-                  <button
-                    className="bg-black text-white m-2 p-1 rounded w-2/3 "
-                    onClick={() => handleRemoveItem(props.item.id)}
-                  >
-                    Remove
-                  </button>
-                ) : (
-                  <button
-                    className="bg-black text-white m-2 p-1 rounded w-2/3 "
-                    onClick={() => handleAddItem(props.item.id)}
-                  >
-                    Add
-                  </button>
-                )}
-                {
-                  wishlistelementfound?(
-                    <button className='bg-red-300 p-1 m-1 rounded-xl' 
-                    onClick={()=>handleRemovefromWishlist(props.item.id)}><GoHeart/></button>
-                  ):
-                  (
-                    <button className=' p-1 m-1 rounded' 
-                    onClick={()=>handleAddtoWishlist(props.item.id)}><GoHeart/></button>
-                    )
-                }
-              
+              {elementFound ? (
+                <button
+                  className="bg-black text-white m-2 p-1 rounded w-2/3 "
+                  onClick={() => handleRemoveItem(props.item.id)}
+                >
+                  Remove
+                </button>
+              ) : (
+                <button
+                  className="bg-black text-white m-2 p-1 rounded w-2/3 "
+                  onClick={() => handleAddItem(props.item.id)}
+                >
+                  Add
+                </button>
+              )}
+              {wishlistelementfound ? (
+                <button
+                  className="bg-red-300 p-1 m-1 rounded-xl"
+                  onClick={() => handleRemovefromWishlist(props.item.id)}
+                >
+                  <GoHeart />
+                </button>
+              ) : (
+                <button
+                  className=" p-1 m-1 rounded"
+                  onClick={() => handleAddtoWishlist(props.item.id)}
+                >
+                  <GoHeart />
+                </button>
+              )}
             </div>
           );
       
