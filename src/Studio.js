b4e0d0822710515fcc14c24346b81a8309cfb430
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-07 23:39:36 +0530

    studio page

--- src/Studio.js
+++ src/Studio.js
@@ -1,11 +1,21 @@
+import { IoMdArrowBack } from "react-icons/io";
+import { useNavigate } from  "react-router-dom";
+
 const Studio = ()=>{
+  const navigate=useNavigate();
+
+  const handleback =()=>{
+    navigate(-1);
+  }
     return (
-      <div className="container m-auto bg-stone-50">
-        <h1 className="text-center p-1 rounded bg-black text-white ">
+      <div className="container m-auto bg-stone-50 w-3/6">
+
+        <h1 className=" p-1 rounded bg-black text-white ">
+        <IoMdArrowBack  className="inline mx-3" onClick={()=>handleback()}/>
           Studio
         </h1>
         <div className="flex flex-wrap justify-center m-auto my-2">
-          <h1>This is Studio Element.</h1>
+          <h1 className="text-sm font-normal">Guide to influencers' Most Covented Looks</h1>
         </div>
       </div>
     );
